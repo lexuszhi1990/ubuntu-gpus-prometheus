312024aba42c45a364eefca77ebe47f9eeaf3ece
david <lexuszhi1990@gmail.com>
2019-01-16 14:56:14 +0800

    add backup5 file

---
+++ backup5.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import prometheus_client as prom
+import pynvml
+import logging
+import time
+
+format = "%(asctime)s - %(levelname)s [%(name)s] %(threadName)s %(message)s"
+logging.basicConfig(level=logging.INFO, format=format)
+
+total_fb_memory = prom.Gauge('gpu_total_fb_memory_mb', 'Total installed frame buffer memory (in ''megabytes)', ['host', 'device'])
+free_fb_memory = prom.Gauge('gpu_free_fb_memory_mb', 'Unallocated frame buffer memory (in ''megabytes)', ['host', 'device'])
+used_fb_memory = prom.Gauge('gpu_used_fb_memory_mb', 'Allocated frame buffer memory (in megabytes).'' Note that the diver/GPU will always set ''a small amount of memory fore bookkeeping.', ['host', 'device'])
+gpu_utilization = prom.Gauge('gpu_utilization_pct', 'Percent of time over the past sample period ''during which one or more kernels was ''executing on the GPU.', ['host', 'device'])
+memory_utilization = prom.Gauge('gpu_mem_utilization_pct', 'Percent of time over the past sample ''period during which global (device) memory ''was being read or written', ['host', 'device'])
+
+with open('/host/etc/hostname', 'r') as f:
+    hostname = f.readline().strip()
+
+pynvml.nvmlInit()
+print('Started with nVidia driver version = %s', pynvml.nvmlSystemGetDriverVersion())
+device_count = pynvml.nvmlDeviceGetCount()
+
+prom.start_http_server(9200)
+
+while True:
+    for i in range(device_count):
+        print('Analyzing device %d...', i)
+
+        handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+
+        print('Querying for memory information...')
+        mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+
+        total_fb_memory.labels(host=hostname, device=i).set(mem_info.total / 1024)
+        free_fb_memory.labels(host=hostname, device=i).set(mem_info.free / 1024)
+        used_fb_memory.labels(host=hostname, device=i).set(mem_info.used / 1024)
+
+        print('Obtaining utilization statistics...')
+        utilization = pynvml.nvmlDeviceGetUtilizationRates(handle)
+
+        gpu_utilization.labels(host=hostname, device=i).set(utilization.gpu / 100.0)
+        memory_utilization.labels(host=hostname, device=i).set(utilization.memory / 100.0)
+        print('Gpu utilization = %s', utilization.gpu)
+        print('Memory utilization = %s', utilization.memory)
+
+        time.sleep(1)
+
+
+# handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+# mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+# print('Memory information = %s', str(mem_info))
+# total_fb_memory.labels(host=hostname, device=gpu_id).set(mem_info.total / 1e9)
+# free_fb_memory.labels(host=hostname, device=gpu_id).set(mem_info.free / 1e9)
+# used_fb_memory.labels(host=hostname, device=gpu_id).set(mem_info.used / 1e9)
+
+# utilization = pynvml.nvmlDeviceGetUtilizationRates(handle)
+# print('Utilization statistics = %s', str(utilization))
+# gpu_utilization.labels(host=hostname, device=gpu_id).set(utilization.gpu / 100.0)
+# memory_utilization.labels(host=hostname, device=gpu_id).set(utilization.memory / 100.0)
+
--- main.py
+++ main.py
@@ -1,62 +1,138 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-import prometheus_client as prom
-import pynvml
+"""
+Pushes nVidia GPU metrics to a Prometheus Push gateway for later collection.
+"""
+import argparse
 import logging
 import time
+import platform
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+from prometheus_client import start_http_server, core
+from pynvml import *
+log = logging.getLogger('nvidia-tool')
+def _create_parser():
+    parser = argparse.ArgumentParser(description='nVidia GPU Prometheus '
+                                                 'Metrics Exporter')
+    parser.add_argument('--verbose',
+                        help='Turn on verbose logging',
+                        action='store_true')
+    parser.add_argument('-u', '--update-period',
+                        help='Period between calls to update metrics, '
+                             'in seconds. Defaults to 30.',
+                        default=30)
+    parser.add_argument('-g', '--gateway',
+                        help='If defined, gateway to push metrics to. Should '
+                             'be in the form of <host>:<port>.',
+                        default=None)
 
-format = "%(asctime)s - %(levelname)s [%(name)s] %(threadName)s %(message)s"
-logging.basicConfig(level=logging.INFO, format=format)
+    parser.add_argument('-p', '--port',
+                        help='If non-zero, port to run the http server',
+                        type=int,
+                        default=0)
 
-total_fb_memory = prom.Gauge('gpu_total_fb_memory_mb', 'Total installed frame buffer memory (in ''megabytes)', ['host', 'device'])
-free_fb_memory = prom.Gauge('gpu_free_fb_memory_mb', 'Unallocated frame buffer memory (in ''megabytes)', ['host', 'device'])
-used_fb_memory = prom.Gauge('gpu_used_fb_memory_mb', 'Allocated frame buffer memory (in megabytes).'' Note that the diver/GPU will always set ''a small amount of memory fore bookkeeping.', ['host', 'device'])
-gpu_utilization = prom.Gauge('gpu_utilization_pct', 'Percent of time over the past sample period ''during which one or more kernels was ''executing on the GPU.', ['host', 'device'])
-memory_utilization = prom.Gauge('gpu_mem_utilization_pct', 'Percent of time over the past sample ''period during which global (device) memory ''was being read or written', ['host', 'device'])
-
-with open('/host/etc/hostname', 'r') as f:
-    hostname = f.readline().strip()
-
-pynvml.nvmlInit()
-print('Started with nVidia driver version = %s', pynvml.nvmlSystemGetDriverVersion())
-device_count = pynvml.nvmlDeviceGetCount()
-
-prom.start_http_server(9200)
-
-while True:
-    for i in range(device_count):
-        print('Analyzing device %d...', i)
-
-        handle = pynvml.nvmlDeviceGetHandleByIndex(i)
-
-        print('Querying for memory information...')
-        mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
-
-        total_fb_memory.labels(host=hostname, device=i).set(mem_info.total / 1024)
-        free_fb_memory.labels(host=hostname, device=i).set(mem_info.free / 1024)
-        used_fb_memory.labels(host=hostname, device=i).set(mem_info.used / 1024)
-
-        print('Obtaining utilization statistics...')
-        utilization = pynvml.nvmlDeviceGetUtilizationRates(handle)
-
-        gpu_utilization.labels(host=hostname, device=i).set(utilization.gpu / 100.0)
-        memory_utilization.labels(host=hostname, device=i).set(utilization.memory / 100.0)
-        print('Gpu utilization = %s', utilization.gpu)
-        print('Memory utilization = %s', utilization.memory)
-
-        time.sleep(0.01)
+    return parser
 
 
-# handle = pynvml.nvmlDeviceGetHandleByIndex(0)
-# mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
-# print('Memory information = %s', str(mem_info))
-# total_fb_memory.labels(host=hostname, device=gpu_id).set(mem_info.total / 1e9)
-# free_fb_memory.labels(host=hostname, device=gpu_id).set(mem_info.free / 1e9)
-# used_fb_memory.labels(host=hostname, device=gpu_id).set(mem_info.used / 1e9)
+def main():
+    parser = _create_parser()
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
-# utilization = pynvml.nvmlDeviceGetUtilizationRates(handle)
-# print('Utilization statistics = %s', str(utilization))
-# gpu_utilization.labels(host=hostname, device=gpu_id).set(utilization.gpu / 100.0)
-# memory_utilization.labels(host=hostname, device=gpu_id).set(utilization.memory / 100.0)
+    registry = core.REGISTRY
 
+    total_fb_memory = Gauge('gpu_total_fb_memory_mb',
+                            'Total installed frame buffer memory (in '
+                            'megabytes)',
+                            ['device', 'hostname'],
+                            registry=registry)
+    free_fb_memory = Gauge('gpu_free_fb_memory_mb',
+                           'Unallocated frame buffer memory (in '
+                           'megabytes)',
+                           ['device', 'hostname'],
+                           registry=registry)
+    used_fb_memory = Gauge('gpu_used_fb_memory_mb',
+                           'Allocated frame buffer memory (in megabytes).'
+                           ' Note that the diver/GPU will always set '
+                           'a small amount of memory fore bookkeeping.',
+                           ['device', 'hostname'],
+                           registry=registry)
+    gpu_utilization = Gauge('gpu_utilization_pct',
+                            'Percent of time over the past sample period '
+                            'during which one or more kernels was '
+                            'executing on the GPU.',
+                            ['device', 'hostname'],
+                            registry=registry)
+    memory_utilization = Gauge('gpu_mem_utilization_pct',
+                               'Percent of time over the past sample '
+                               'period during which global (device) memory '
+                               'was being read or written',
+                               ['device', 'hostname'],
+                               registry=registry)
+
+    iteration = 0
+    with open('/host/etc/hostname', 'r') as f:
+        hostname = f.readline().strip()
+
+    try:
+        log.debug('Initializing NVML...')
+        nvmlInit()
+        print("Initialized")
+
+        log.info('Started with nVidia driver version = %s',
+                 nvmlSystemGetDriverVersion())
+
+        device_count = nvmlDeviceGetCount()
+        log.info('%d devices found.', device_count)
+
+
+        # log.info('Starting http server on port %d', 8080)
+        # start_http_server(8080)
+        # log.info('HTTP server started on port %d', 8080)
+
+        while True:
+
+            iteration += 1
+            log.info('Current iteration = %d', iteration)
+
+            for i in range(device_count):
+                log.info('Analyzing device %d...', i)
+                try:
+                    # log.info('Obtaining handle for device %d...', i)
+                    handle = nvmlDeviceGetHandleByIndex(i)
+                    # log.info('Device handle for %d is %s', i, str(handle))
+
+                    # log.info('Querying for memory information...')
+                    mem_info = nvmlDeviceGetMemoryInfo(handle)
+                    # log.info('Memory information = %s', str(mem_info))
+
+                    total_fb_memory.labels(device=i, hostname=hostname).set(mem_info.total)
+                    free_fb_memory.labels(device=i, hostname=hostname).set(mem_info.free)
+                    used_fb_memory.labels(device=i, hostname=hostname).set(mem_info.used)
+
+                    # log.info('Obtaining utilization statistics...')
+                    utilization = nvmlDeviceGetUtilizationRates(handle)
+                    # log.info('Utilization statistics = %s', str(utilization))
+
+                    gpu_utilization.labels(device=i, hostname=hostname).set(utilization.gpu / 100.0)
+                    memory_utilization.labels(device=i, hostname=hostname).set(utilization.memory / 100.0)
+                except Exception as e:
+                    log.info(e, exc_info=True)
+
+                log.info('Pushing metrics to gateway at %s...', args.gateway)
+                push_to_gateway('my-prom-prometheus-pushgateway:9091', job=platform.node(), registry=core.REGISTRY)
+                # hostname = platform.node()
+                # print(hostname)
+                # print(core.REGISTRY)
+                log.info('Push complete.')
+
+            time.sleep(5)
+
+    except Exception as e:
+        log.info('Exception thrown - %s', e, exc_info=True)
+    finally:
+        nvmlShutdown()
+
+
+if __name__ == '__main__':
+    main()
